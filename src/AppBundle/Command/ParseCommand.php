15498ea27aaaa5d3271c8dc1c085b7c1569a3ac1
iGor3kUA <igor3k.bondarenko@gmail.com>
2017-08-29 21:57:12 +0300

    commit before parsing namespaces...

--- src/AppBundle/Command/ParseCommand.php
+++ src/AppBundle/Command/ParseCommand.php
@@ -10,15 +10,9 @@ class ParseCommand extends Command
     protected function configure()
     {
         $this
-            // the name of the command (the part after "bin/console")
             ->setName('app:parse')
-
-            // the short description shown while running "php bin/console list"
-            ->setDescription('Parse...')
-
-            // the full command description shown when running the command with
-            // the "--help" option
-            ->setHelp('This command allows you to create a user...')
+            ->setDescription('Parse api.symfony.com')
+            ->setHelp('This command allows you to parse api.symfony.com and save namespaces to database')
         ;
     }
 
