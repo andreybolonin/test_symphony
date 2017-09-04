af4c23dcceac2b01e32b7a9a63d15fdfbd0910b2
iGor3kUA <igor3k.bondarenko@gmail.com>
2017-09-04 12:44:32 +0300

    saving namespaces only...

--- src/AppBundle/Command/ParseCommand.php
+++ src/AppBundle/Command/ParseCommand.php
@@ -1,11 +1,17 @@
 <?php
 namespace AppBundle\Command;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use AppBundle\Entity\InterfaceSymfony;
+use AppBundle\Entity\ClassSymfony;
+use AppBundle\Entity\NamespaceSymfony;
+use Symfony\Component\DomCrawler\Crawler;
 
-class ParseCommand extends Command
+class ParseCommand extends ContainerAwareCommand
 {
     protected function configure()
     {
@@ -18,6 +24,20 @@ class ParseCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $html = file_get_contents('http://api.symfony.com/3.2/index.html');
+        $crawler = new Crawler($html);
+        $em = $this->getContainer()->get('doctrine')->getManager();
 
+        $namepsaces = $crawler->filter('div.namespace-container > ul > li > a');
+        foreach ($namepsaces as $item) {
+            $url = 'http://api.symfony.com/3.2/'.$item->getAttribute("href");
+            $name = $item->textContent;
+
+            $namespace = new NamespaceSymfony();
+            $namespace->setUrl($url);
+            $namespace->setName($name);
+            $em->persist($namespace);
+        }
+        $em->flush();
     }
 }
