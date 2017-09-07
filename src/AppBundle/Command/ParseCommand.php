7030c108b68aea071737f4171d0680626a2a7cb2
iGor3kUA <igor3k.bondarenko@gmail.com>
2017-09-07 11:52:48 +0300

    rewritten with a recursive function

--- src/AppBundle/Command/ParseCommand.php
+++ src/AppBundle/Command/ParseCommand.php
@@ -17,31 +17,33 @@ class ParseCommand extends ContainerAwareCommand
     {
         $this
             ->setName('app:parse')
-            ->setDescription('Parse api.symfony.com')
-            ->setHelp('This command allows you to parse api.symfony.com and save namespaces to database')
+            ->setDescription('Parse api.symfony.com and save to Database')
+            ->setHelp('This command allows you to parse api.symfony.com and save namespaces/classes/interfaces to database')
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $html = file_get_contents('http://api.symfony.com/3.2/');
+        $this->getNamespaces('http://api.symfony.com/3.2/Symfony.html');
+    }
+    public function getNamespaces ($url)
+    {
+        $html = file_get_contents($url);
         $crawler = new Crawler($html);
         $em = $this->getContainer()->get('doctrine')->getManager();
-        $namepsaces = $crawler->filter('div.namespace-container > ul > li > a');
+        $namepsaces = $crawler->filter('div.namespace-list > a');
+        $classes = $crawler->filter('div.row > div.col-md-6 > a');
+        $interfaces = $crawler->filter('div.col-md-6 > em > a');
 
         foreach ($namepsaces as $itemNamespace) {
-            $namespaceUrl = 'http://api.symfony.com/3.2/'.$itemNamespace->getAttribute("href");
+            $namespaceUrl = 'http://api.symfony.com/3.2/'.str_replace("../", "", $itemNamespace->getAttribute("href"));
             $namespaceName = $itemNamespace->textContent;
 
             $namespace = new NamespaceSymfony();
             $namespace->setUrl($namespaceUrl);
             $namespace->setName($namespaceName);
             $em->persist($namespace);
-
-            $htmlNamespaceForClass = file_get_contents($namespaceUrl);
-            $crawlerClass = new Crawler($htmlNamespaceForClass);
-
-            $classes = $crawlerClass->filter('div.container-fluid.underlined > div.row > div.col-md-6 > a');
+            $this->getNamespaces($namespaceUrl);
 
             foreach ($classes as $itemClass) {
                 $classUrl = 'http://api.symfony.com/3.2/'.str_replace("../", "", $itemClass->getAttribute("href"));
@@ -54,22 +56,16 @@ class ParseCommand extends ContainerAwareCommand
                 $em->persist($class);
             }
 
-            $htmlNamespaceForInterface = file_get_contents($namespaceUrl);
-            $crawlerInterface = new Crawler($htmlNamespaceForInterface);
-
-            $interfaces = $crawlerInterface->filter('div.container-fluid.underlined > div.row > div.col-md-6 > em > a');
-
             foreach ($interfaces as $itemInterface) {
-                $interfaceUrl = 'http://api.symfony.com/3.2/'.str_replace("../", "", $itemInterface->getAttribute("href"));
+                $interfaceUrl = 'http://api.symfony.com/3.2/' . str_replace("../", "", $itemInterface->getAttribute("href"));
                 $interfaceName = $itemInterface->textContent;
                 $interface = new InterfaceSymfony();
-
                 $interface->setUrl($interfaceUrl);
                 $interface->setName($interfaceName);
                 $interface->setNamespaceSymfony($namespace);
                 $em->persist($interface);
             }
+            $em->flush();
         }
-        $em->flush();
     }
 }
